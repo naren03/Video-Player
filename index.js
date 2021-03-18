c33d156702b2fd4022c6909b22aa91a792cf66d8
Narendra <naren3001singh@gmail.com>
2021-03-18 22:22:26 +0530

    Initial Commit Basic Functionality done

---
+++ index.js
@@ -0,0 +1,73 @@
+const video = document.getElementById('video');
+const toggle = document.getElementById('toggle');
+const timestamp = document.getElementById('timestamp');
+const progress = document.getElementById('progress');
+
+// play and pause using button
+toggle.addEventListener('click', toggleBtn);
+//play and pause directly clicking on video
+video.addEventListener('click', directToggle);
+
+//update timestamp
+video.addEventListener('timeupdate', changeTime);
+
+//progress bar
+progress.addEventListener('change', changeProgress);
+
+function directToggle() {
+	if (toggle.classList.contains('fa-play')) {
+		video.play();
+		toggle.classList.remove('fa-play');
+		toggle.classList.add('fa-pause');
+	} else {
+		video.pause();
+		toggle.classList.remove('fa-pause');
+		toggle.classList.add('fa-play');
+	}
+}
+
+function toggleBtn(e) {
+	if (e.target.classList.contains('fa-play')) {
+		video.play();
+		e.target.classList.remove('fa-play');
+		e.target.classList.add('fa-pause');
+	} else {
+		video.pause();
+		e.target.classList.remove('fa-pause');
+		e.target.classList.add('fa-play');
+	}
+}
+
+function changeTime() {
+	let curMins = Math.floor(video.currentTime / 60);
+	let curSecs = Math.floor(video.currentTime % 60);
+	let totalMins = Math.floor(video.duration / 60);
+	let totalSecs = Math.floor(video.duration % 60);
+
+	if (curMins < 10) {
+		curMins = '0' + curMins;
+	}
+	if (curSecs < 10) {
+		curSecs = '0' + curSecs;
+	}
+	if (totalMins < 10) {
+		totalMins = '0' + totalMins;
+	}
+	if (totalSecs < 10) {
+		totalSecs = '0' + totalSecs;
+	}
+
+	//updating timestamp continously
+	timestamp.textContent = `${curMins}:${curSecs}/${totalMins}:${totalSecs}`;
+
+	//update the progress bar
+	updateProgress((video.currentTime / video.duration) * 100);
+}
+
+function updateProgress(curValue) {
+	progress.value = curValue;
+}
+
+function changeProgress() {
+	video.currentTime = (progress.value * video.duration) / 100;
+}
